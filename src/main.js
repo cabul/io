223fe566c5ec76ed1fcd365af82ba4165884b888
Calvin Bulla <calvin.bulla@gmail.com>
2014-09-11 15:24:02 +0100

    Mouse Mage

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ function init() {
   document.body.appendChild(renderer.domElement);
   
   camera = new THREE.PerspectiveCamera(45,WIDTH/HEIGHT,0.1,20000);
-  camera.position.set(5,10,20);
+  camera.position.set(20,14,12);
   scene.add(camera);
   
   window.addEventListener('resize', function(){
@@ -26,12 +26,18 @@ function init() {
   
   renderer.setClearColor(0x333F47, 1);
   var light = new THREE.PointLight(0xffffff);
-  light.position.set(-100,200,100);
+  light.position.set(100,200,100);
   scene.add(light);
+  var ambient = new THREE.AmbientLight(0x707070);
+  scene.add(ambient);
   
   var loader = new THREE.JSONLoader();
-  loader.load("res/humanoid.js",function(geometry) {
-    var material = new THREE.MeshLambertMaterial({color: 0x55B663});
+  loader.load("res/mouse.js",function(geometry,materials) {
+    console.log(arguments);
+    var material = new THREE.MeshFaceMaterial(materials);
+    for(var i = 0, len=materials.length;i<len;++i) {
+      materials[i].shading = THREE.FlatShading;
+    }
     mesh = new THREE.Mesh(geometry,material);
     scene.add(mesh);
   });
